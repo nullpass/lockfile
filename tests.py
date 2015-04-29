6a4af134a10ca26453ac7e8c3a13ca71db413e32
nullpass <nullpass@gmail.com>
2015-04-29 11:13:21 -0400

    Wed Apr 29 15:13:21 UTC 2015

--- tests.py
+++ tests.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-from __future__ import absolute_import
 import locker
 
 
@@ -7,7 +6,7 @@ def main():
     print('#### instance')
     # a = locker.Locker(file='./foo.pid', age_limit=1)
     # a = locker.Locker(age_limit=0)
-    a = locker.Locker(file='./foo.pid', age_limit=0)
+    a = locker.Locker(file='/tmp/foo.pid', age_limit=10)
     # a = locker.Locker(file='./foo.pid', age_limit=0, kill=True)
 
     print('#### try create')
